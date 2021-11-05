582a5e225b2c10a36bc2a4462d096bb67d874dbb
Siraj <sirajkhanc2305@gmail.com>
2021-11-05 13:23:05 +0530

    Added: Created Fare class for calculating the fare

--- src/main/java/com/bridgelabz/Fare.java
+++ src/main/java/com/bridgelabz/Fare.java
@@ -1,4 +1,40 @@
 package com.bridgelabz;
 
+/**
+ * @author -> Siraj
+ * @version -> 1.0
+ * @since  -> 05/11/2021
+ */
 public class Fare {
+
+    /**
+     * This program generates fares for Cabs
+     */
+    private double totalFare;           // Stores the total fare for all the ride combined.
+
+    public Fare(double distance, double minutes) {
+        fareCalculator(distance, minutes);
+    }
+
+    public Fare() {
+    }
+
+    /**
+     *
+     * @param distance -> The amount of distance travelled by the customer per ride.
+     * @param minutes -> The amount of time taken for the ride.
+     */
+    private void fareCalculator(double distance, double minutes) {
+        int MIN_FARE = 5;
+        int PER_MINUTE = 1;
+        totalFare += ((distance * 10) + (PER_MINUTE * minutes) + MIN_FARE);
+    }
+
+    /**
+     *
+     * @return total combined fare for all the rides of customer.
+     */
+    public double getTotalFare() {
+        return totalFare;
+    }
 }
