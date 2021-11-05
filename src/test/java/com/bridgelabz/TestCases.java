a7e624cca19c8edd80c84e397a7bdbb080ea9313
Siraj <sirajkhanc2305@gmail.com>
2021-11-05 13:15:29 +0530

    Added: TestCase for basic fare calculations

--- src/test/java/com/bridgelabz/TestCases.java
+++ src/test/java/com/bridgelabz/TestCases.java
@@ -1,4 +1,13 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestCases {
+
+    @Test
+    public void whenGiven10KmAnd4MinutesAndFareAs109_shouldReturnEqual() {
+        Fare fare = new Fare(10,4);
+        Assert.assertEquals(fare.getTotalFare(),109,0.5);
+    }
 }
